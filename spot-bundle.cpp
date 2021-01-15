fdc2c60c8819bf59a080500b4a07375e72d7eaac
David Poliakoff <david.poliakoff@gmail.com>
2021-01-15 06:50:41 -0800

    Added metadata callback

--- spot-bundle.cpp
+++ spot-bundle.cpp
@@ -2,8 +2,11 @@
 #include <adiak.hpp>
 #include <iostream>
 
-extern "C" void kokkosp_push_region();
+extern "C" void kokkosp_push_region(const char*);
 extern "C" void kokkosp_pop_region();
+extern "C" void kokkosp_declare_metadata(const char* key, const char* value){
+  adiak::value(key,value);
+}
 __attribute__((constructor)) void prepare_list_globals(){
     adiak::user();
     adiak::launchdate();
