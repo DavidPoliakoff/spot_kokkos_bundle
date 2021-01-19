34597e56022aa219e992d6af075dcf85395ad987
David Poliakoff <david.poliakoff@gmail.com>
2021-01-19 15:19:27 -0800

    Try float parsing

--- spot-bundle.cpp
+++ spot-bundle.cpp
@@ -5,6 +5,23 @@
 extern "C" void kokkosp_push_region(const char*);
 extern "C" void kokkosp_pop_region();
 extern "C" void kokkosp_declare_metadata(const char* key, const char* value){
+  std::string parseme(value);
+  try {
+    size_t end;
+    float v = std::stof(parseme,&end);
+    adiak::value(key, v);
+    return;
+  }
+  catch(std::invalid_argument){
+  }
+  try {
+    size_t end;
+    int i = std::stoi(parseme,&end);
+    adiak::value(key, i);
+    return;
+  }
+  catch(std::invalid_argument){
+  }
   adiak::value(key,value);
 }
 __attribute__((constructor)) void prepare_list_globals(){
